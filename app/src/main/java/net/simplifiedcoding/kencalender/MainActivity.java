cf74ce7618ba894790839a8d1f55cb875283a87d
Eli Kiprop <109068401+elikiprop@users.noreply.github.com>
2024-03-12 14:50:27 +0300

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/kencalender/MainActivity.java
@@ -0,0 +1,59 @@
+package net.simplifiedcoding.kencalender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView displayDate;
+    private Calendar calendar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.pickDate);
+        displayDate = findViewById(R.id.displayDate);
+        calendar = Calendar.getInstance(); // Initialize calendar instance
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, day);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()); // corrected pattern from "dd/mm/yyyy" to "dd/MM/yyyy"
+            String selectedDate = dateFormat.format(calendar.getTime());
+            displayDate.setText(selectedDate);
+
+        }
+    };
+}
